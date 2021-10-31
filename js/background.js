6ddd5cd3a3096cd94f3adcb430592285f3aaea2f
buuuuri <dlwngml0204@naver.com>
2021-10-31 23:18:52 +0900

    수정

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `./img/${chosenImage}`;
 
 //prepend도 가능 => 가장 위에 오게 하는거
 document.body.appendChild(bgImage); //가장 뒤에 오게하는거
